a326da272938a34c92052c702e51e7fce307c03a
sanjana8100 <sanjanams812@gmail.com>
2023-03-04 11:07:08 +0530

    added:UC1

--- Start.java
+++ Start.java
@@ -3,6 +3,14 @@ package com.bridgelabz;
 public class Start {
     public static void main(String[] args) {
 
-        System.out.println(" Welcome to employee wage");
+        System.out.println("Welcome to Employee Wage Computation Program On Master Branch");
+        System.out.println("Checking whether Employee is present or absent...");
+        int empPresent = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if(empCheck == empPresent){
+            System.out.println("Employee is Present");
+        }
+        else
+            System.out.println("Employee is Absent");
     }
 }
